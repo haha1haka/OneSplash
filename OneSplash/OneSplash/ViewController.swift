5acd6aaa2480d6b6878df02dbc3c05f4995b9d17
haha1haka <haha1haka@icloud.com>
2022-10-20 16:18:46 +0900

    [Feat] 프로젝트 세

---
+++ OneSplash/OneSplash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OneSplash
+//
+//  Created by HWAKSEONG KIM on 2022/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
